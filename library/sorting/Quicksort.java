752a84382a84f2a2f509d3c16d2ce07a89509a90
juanto121 <juanf.torres.lopez@gmail.com>
2019-05-11 13:46:43 -0500

    Quicksort implementation

--- library/sorting/Quicksort.java
+++ library/sorting/Quicksort.java
@@ -4,27 +4,40 @@ public class Quicksort {
     public static void main(String[] args) {
         //int a[] = {5,4,3,2,1};
         //int a[] = {5,4};
-        //int a[] = {1,2,3,6,67,87,1,5,3,2,1,345,6};
-
-        int a[] = {1,2,3,6,67,87,68,32,4609,87,67,31,55,68,99,33,5,3,2,345,6};
+        int a[] = {1,2,3,6,87,1,2,1};
+        //int a[] = {1,2,3,6,67,87,68,32,4609,87,67,31,55,68,99,33,5,3,2,345,6};
+        quickSort(a,0, a.length-1);
         qsort(a, 0, a.length-1);
         System.out.println(Arrays.toString(a));
+        int b[] = {1,2,3,6,67,87,68,32,4609,87,67,31,55,68,99,33,5,3,2,345,6};
+        quickSort(b, 0, b.length-1);
+        //System.out.println(Arrays.toString(b));
     }
     private static void qsort(int[] a, int left, int right) {
         if(left < right) {
             int partition = partition(a, left, right);
-            if(left < partition)
-                qsort(a, left, partition - 1);
-            if(right > partition)
-                qsort(a, partition+1, right);
+            printarr(a, partition);
+            qsort(a, left, partition - 1);
+            qsort(a, partition, right);
         }
+    }
+
+    private static void printarr(int[] a, int partition) {
+
+        for(int i = 0; i <  a.length; i++){
+            if(i==partition)
+                System.out.print("["+a[i]+"]" + " ");
+            else
+                System.out.print(a[i] + " ");
+        }
+        System.out.println("");
     }
 
     private static int partition(int[] a, int left, int right) {
         int mid = ( left + right ) / 2;
         int pivot = a[mid];
 
-        while( left < right) {
+        while( left <= right) {
 
             while(a[left] < pivot){
                 left++;
@@ -41,9 +54,51 @@ public class Quicksort {
                 left++;
                 right--;
             }
+
         }
 
         return left;
+    }
+
+
+    // Taken from https://www.programcreek.com/2012/11/quicksort-array-in-java/
+    public static void quickSort(int[] arr, int low, int high) {
+        if (arr == null || arr.length == 0)
+            return;
+
+        if (low >= high)
+            return;
+
+        // pick the pivot
+        int middle = low + (high - low) / 2;
+        int pivot = arr[middle];
+
+        // make left < pivot and right > pivot
+        int i = low, j = high;
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+
+            while (arr[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        // recursively sort two sub parts
+        if (low < j)
+            quickSort(arr, low, j);
+
+        if (high > i)
+            quickSort(arr, i, high);
     }
 
 
